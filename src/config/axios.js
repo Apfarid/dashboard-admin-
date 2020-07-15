f84ea35696600e58869751be1a94e9af2702b692
Anderson Palacio <Anderson.Palacio@endava.com>
2020-07-15 14:38:19 -0500

    last

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from "axios";
 
 const clienteAxios = axios.create({
-    baseURL: 'http://localhost:3500'
-})
+  baseURL: process.env.REACT_APP_BACKEND,
+});
 
-export default clienteAxios
+export default clienteAxios;
